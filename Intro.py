56e83a20f4a09011306b8ebfb4afe575b83ff810
Santiago Gontier <INSTRUCTOR@PERDFPCSCFOD426.FormacionSena.RED>
2024-10-09 17:53:16 -0500

    first.2

--- Intro.py
+++ Intro.py
@@ -33,16 +33,38 @@
 # print(f"\Texto original es = {texto}")
 # print(f"\Texto original es = {texto_split}")
 # print(f"texto unido = {unido}")
-a = 2
-b = 5
-if 1==1:
-    print ("SI")
-else: 
-    print ("NO")
-###### elseif ####
-if a<b:
-    print (f"{a} es menor que {b}")
-elif b==a:
-    print (f"{a} es igual que {b}")
-else:
-    print (f"{a} mayor que {b}")
+# a = 2
+# b = 5
+# if 1==1:
+#     print ("SI")
+# else: 
+#     print ("NO")
+# ###### elseif ####
+# if a<b:
+#     print (f"{a} es menor que {b}")
+# elif b==a:
+#     print (f"{a} es igual que {b}")
+# else:
+#     print (f"{a} mayor que {b}")
+###For loop####
+#               #start, stop, step 
+# for i in range(2,11,2):
+#     print(i)
+# for l in 'texto':
+#     print(l)
+# for j in range (1):
+#     for k in 'hola':
+#         print (j,k)
+############### WHILE LOOP ############
+######## Lists #######
+list=[]
+lista=['ole', 2, 3,'vio']
+#### Slicing ####
+nueva_lista = lista[2:]#Si no especifico nada en el final está seguira hasta el fin de la lista y si no especifico nada cogerá todo
+print (f"Lista [0:3] = {lista[0:3]}")
+# print (f"{dir(list)}")
+###### Lista anidadas ############
+lista3=['nice', lista, 100, 54]
+print (lista3)
+elemento= lista3[1][2]
+print (f"elemento = {elemento}")
